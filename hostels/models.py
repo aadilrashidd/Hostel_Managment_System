7fbbe534d88f65fd7c5040e0bf7d413b337def26
aadilrashidd <146063398+aadilrashidd@users.noreply.github.com>
2023-11-12 23:31:33 +0500

    Hostels app and room model created

--- hostels/models.py
+++ hostels/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Hostel(models.Model):
+    name=models.CharField( max_length=50)
+    hostel_address=models.CharField( max_length=50)
+    created_at=models.DateField( auto_now=True, auto_now_add=False)
+    modifide_at=models.DateTimeField( auto_now=False, auto_now_add=True)
+   
+class Room(models.Model):
+    hostel=models.ForeignKey("hostels.Hostel",on_delete=models.CASCADE)    
+    room_number=models.CharField( max_length=50)
+    number_of_seats=models.IntegerField()
+    attached_washroom=models.BooleanField()
+    remarks=models.TextField()
+    created_at=models.DateField( auto_now=True, auto_now_add=False)
+    modifide_at=models.DateTimeField( auto_now=False, auto_now_add=True)
+    
